9491667ad8a1d9d2f89543df53d32f914d70a70f
lcm40mill <lcm40mill@gmail.com>
2025-10-24 21:57:42 -0500

    TextNode class creation

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode
+def main():
+    node1 = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(node1)
+
+if __name__ == "__main__":
+    main()
