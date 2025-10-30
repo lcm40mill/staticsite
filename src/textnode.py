1afff016033f14df4f4d5b33702051c2f6670b9b
lcm40mill <lcm40mill@gmail.com>
2025-10-30 16:31:41 -0500

    text to html added

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TextType(Enum):
@@ -25,3 +26,20 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(tag=None, value=text_node.text)
+        case TextType.BOLD:
+            return LeafNode(tag="b", value=text_node.text)
+        case TextType.ITALIC:
+            return LeafNode(tag="i", value=text_node.text)
+        case TextType.CODE:
+            return LeafNode(tag="code", value=text_node.text)
+        case TextType.LINK:
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode(tag="image", value="", props={"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception(f"Invalid text type: {text_node.text_type}")
