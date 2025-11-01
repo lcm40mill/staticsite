e1d00740720a93d4c06082e3e49d2c59be3baa41
lcm40mill <lcm40mill@gmail.com>
2025-11-01 11:13:57 -0500

    text split links & images

--- src/codework.py
+++ src/codework.py
@@ -1,4 +1,5 @@
 import re
+import os
 from textnode import TextNode, TextType
 
 
@@ -31,3 +32,67 @@ def extract_markdown_links(text):
     pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern, text)
     return matches
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type  != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        matches = extract_markdown_links(text)
+
+        if len(matches) == 0:
+            new_nodes.append(node)
+            continue
+
+        for link in matches:
+            
+            sections = text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, link not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link [1]))
+            
+            text = sections[1]
+        
+        if text != "":
+            new_nodes.append(TextNode(text, TextType.TEXT))
+
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type  != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        matches = extract_markdown_images(text)
+
+        if len(matches) == 0:
+            new_nodes.append(node)
+            continue
+
+        for image in matches:
+            
+            sections = text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
+            text = sections[1]
+        if text != "":
+            new_nodes.append(TextNode(text, TextType.TEXT))
+
+    return new_nodes
